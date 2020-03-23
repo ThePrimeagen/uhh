3de6c8109e23704af309fe38a412ec85d02c457e
ThePrimeagen <the.primeagen@gmail.com>
2020-03-23 09:22:27 -0700

    gobroke(git2)

--- src/setup/setup.h
+++ src/setup/setup.h
@@ -3,16 +3,16 @@
 #include <fstream>
 #include <string>
 
-    class Uhh {
-        public:
-            std::string dir;
-            std::string configPath;
-            std::string repoPath;
+class Uhh {
+    public:
+        std::string dir;
+        std::string configPath;
+        std::string repoPath;
 
-            Uhh(std::string& base);
-            ~Uhh();
+        Uhh(std::string& base);
+        ~Uhh();
 
-        private:
-            bool uninitialized;
+    private:
+        bool uninitialized;
 };
 
--- src/uhh.cpp
+++ src/uhh.cpp
@@ -1,4 +1,5 @@
 #include "setup/setup.h"
+#include "git2.h"
 
 int main(int argc, char** argv) {
     (void)argc;
